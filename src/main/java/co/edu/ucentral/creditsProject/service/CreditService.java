93c29b6a2df2623540afedbb2543ef9973a1fadb
Nicolas <ngomezr2@ucentral.edu.co>
2024-04-23 16:31:12 -0500

    adicion metodo en service para registrar credito

--- src/main/java/co/edu/ucentral/creditsProject/service/CreditService.java
+++ src/main/java/co/edu/ucentral/creditsProject/service/CreditService.java
@@ -1,7 +1,10 @@
 package co.edu.ucentral.creditsProject.service;
 
+import co.edu.ucentral.creditsProject.dto.Credit;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface CreditService {
+    public Credit registerCredit(Credit credit);
+
 }
