1e0af908de1f6c0c1d25867b9490e1adb255a6b5
nicolas gomez <ngomezr2@ucentral.edu.co>
2024-05-21 19:58:47 -0500

    -cambio en switches de CreditAdapter para mejor lectura de codigo -se agrega simulacion de credito para que el usuario pueda ver el valor de una cuota

--- src/main/java/co/edu/ucentral/creditsProject/controller/CreditController.java
+++ src/main/java/co/edu/ucentral/creditsProject/controller/CreditController.java
@@ -5,9 +5,7 @@ import co.edu.ucentral.creditsProject.service.CreditService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CreditController {
@@ -15,18 +13,34 @@ public class CreditController {
     @Autowired
     CreditService creditService;
 
+    boolean firstTimeRequest = true;
+
     @GetMapping("/credit")
     public String mostrarFormulario(Model model){
         Credit credit = new Credit();
         model.addAttribute("credit", credit);
+        firstTimeRequest = true;
+        model.addAttribute("firstTime", firstTimeRequest);
+
         return "radicarCredito";
     }
 
 
     @PostMapping("/credit")
-    public String registerCredit(@ModelAttribute("credit") Credit credit) {
-        creditService.registerCredit(credit);
-        return "redirect:/";
+    public String registerCredit(@ModelAttribute("credit") Credit credit, Model model) {
+        if(firstTimeRequest){
+            model.addAttribute("credit", credit);
+            firstTimeRequest = false;
+            model.addAttribute("firstTime", firstTimeRequest);
+            model.addAttribute("fee",creditService.quotesimulation(credit.getTotalAmount(),creditService.getInterest(creditService.getCreditType(credit.getType())),credit.getMonthsTime()));
+            return "radicarCredito";
+        }else{
+            firstTimeRequest = true;
+            creditService.registerCredit(credit);
+            return "redirect:/";
+        }
+
     }
+
 
 }
